fb00f5c258cb683f0eaf506a2a16aecc9615da45
pavanimajjiga <108386233+pavanimajjiga@users.noreply.github.com>
2022-07-07 11:38:33 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+	public static void main(String args[]){
+		int myNumber=0;
+		char myChar='C';
+		boolean myBool= false; // true
+		System.out.println("Integer Value " + myNumber + " . " );
+		System.out.print("Boolean value " + myBool + " .\n");
+		
+		System.out.printf("My Char Value " + myChar);
+	}
+}
